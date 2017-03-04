76f4fe5d1580369ed280011f9ffeb15480c81a73
Bin Wang <bw2491@columbia.edu>
2017-03-03 22:32:14 -0500

    add

--- app/models.py
+++ app/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# notes-project/app/models.py
+
+import os
+import datetime
+# import mongoengine
+from mongoengine import Connect, Document, StringField, DateTimeField
+# get the URI from environment variables
+# uri = os.getenv('URI')
+# connect to our database at AWS
+# connect(host=uri)
+uri = 'test'
+connect(uri)
+
+class Notebook(Document):
+    '''
+    this collection defines how our notes document will look like
+    '''
+    title = StringField(max_length=500, required=True) # Title of the note
+    notes = StringField(max_length=500, required=True) # Details of the note
+    date_modified = DateTimeField(default=datetime.datetime.now)
+
+
+
+
+
