847c17a3abc895a234a648b6e6834f0e73855297
Bin Wang <bw2491@columbia.edu>
2017-03-03 23:36:34 -0500

    finishing note-view

--- app/views.py
+++ app/views.py
@@ -6,4 +6,16 @@ from django.shortcuts import render, redirect
 from .models import Notebook
 
 def index(request):
-        pass
+        # handle form submission
+        if request.method = 'POST':
+            title = request.POST.get('title')
+            notes = requeset.POST.get('notes')
+        # prepare the notes before saving to database
+        notebook = Notebook(title=title, notes=notes)
+        notebook.save()
+        # redirect to homepage after saving
+        return redirect('/')
+    # all get requests will be handled here
+    notes = Notebook.objects()
+    return render(request,"index.html",{"context":notes},)
+
